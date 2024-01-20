250b8cf7ae030fc4008b03ec4612541e6d07fbf5
LaluPrasad-M <laluprasad.73515274@gmail.com>
2024-01-21 02:39:19 +0530

    added alternate forRootAsync for forRoot so that either format can be used based on the need

--- src/bullMQ/Configs/get-bull-module.ts
+++ src/bullMQ/Configs/get-bull-module.ts
@@ -10,3 +10,31 @@ export function getBullModule(): DynamicModule {
     },
   });
 }
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+``` 
+// Replace the above synchronous Code with the Below Code to make it Async
+
+import { BullModule } from '@nestjs/bull';
+import { DynamicModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export function getBullModule(): DynamicModule {
+  return BullModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => ({
+      redis: {
+        host: configService.get<string>('REDIS_HOST', 'localhost'),
+        port: configService.get<number>('REDIS_PORT', 6379),
+        password: configService.get<string>('REDIS_PASSWORD', ''),
+      },
+      limiter: {
+        max: configService.get<number>('BULL_LIMITER_MAX', 24),
+        duration: configService.get<number>('BULL_LIMITER_DURATION', 3000),
+      },
+    }),
+  });
+}
+```;
