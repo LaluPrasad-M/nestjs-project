b7edd9ef1864872da9e7dffdf33ebd6c602281d5
LaluPrasad-M <laluprasad.73515274@gmail.com>
2024-02-25 19:36:33 +0530

    SSE: Added Api description for swagger doc

--- src/serverSentEventsApp/sse.controller.ts
+++ src/serverSentEventsApp/sse.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Sse, Post, Body } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { SseService } from './sse.service';
+import { ApiBody } from '@nestjs/swagger';
 
 const CHANNEL = 'test';
 
@@ -16,6 +17,7 @@ export class SseController {
     return this.sseService.sseSubscribe(CHANNEL);
   }
 
+  @ApiBody({ type: Object })
   @Post('emitEvent')
   async postSubscribe(@Body() body: Record<string, unknown>): Promise<void> {
     return this.sseService.postSubscribe(CHANNEL, body);
